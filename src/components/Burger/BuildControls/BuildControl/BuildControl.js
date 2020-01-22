99092e05d973d2c4e833ebd3645aabe9307c8618
unknown <jakub@wirtualnafotografia.pl>
2020-01-22 14:29:52 +0100

    fixed bad import path in app.js

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,7 +3,7 @@ import classes from './BuildControl.module.scss';
 
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
-        <div className={classes.Label}> {props.label}</div>
+        <div className={classes.Label}>{props.label}</div>
         <button 
         className={classes.Less} 
         onClick={props.removed} 
